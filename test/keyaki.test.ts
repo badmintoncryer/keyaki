735900335acf94ff3e33d2f5483b74250699531a
Kazuho CryerShinozuka <malaysia.cryer@gmail.com>
2025-01-19 14:25:42 +0900

    initial commit

---
+++ test/keyaki.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Keyaki from '../lib/keyaki-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/keyaki-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Keyaki.KeyakiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
